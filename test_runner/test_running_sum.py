7128c7c8aacbd46757b9a47f4657a9ec9f4a3d59
CharlesPlachno <charlesplachno@outlook.com>
2022-06-22 23:05:28 -0700

    took out unused import from running_sum

--- test_runner/test_running_sum.py
+++ test_runner/test_running_sum.py
@@ -3,7 +3,6 @@ a set of input/output tests for the running_sums function
 '''
 
 from problems.running_sum import running_sums
-import pytest
 
 def test_example_one():
     assert running_sums([1,2,3,4]) == [1,3,6,10]
