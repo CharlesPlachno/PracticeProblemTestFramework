447969e0302cf6bd7b25e34bcb4b91843a9a5fdd
CharlesPlachno <charlesplachno@outlook.com>
2022-06-22 22:24:16 -0700

    fixed typo

--- test_runner/test_running_sum.py
+++ test_runner/test_running_sum.py
@@ -6,10 +6,10 @@ from problems.running_sum import running_sums
 import pytest
 
 def test_example_one():
-    assert running_sums([1,2,3,4] == [1,3,6,10])
+    assert running_sums([1,2,3,4]) == [1,3,6,10]
 
 def test_example_two():
-    assert running_sums([1,1,1,1,1] == [1,2,3,4,5])
+    assert running_sums([1,1,1,1,1]) == [1,2,3,4,5]
 
 def test_example_three():
-    assert running_sums([3,1,2,10,1] == [3,4,6,16,17])
+    assert running_sums([3,1,2,10,1]) == [3,4,6,16,17]
