0f2a65b4a6a1c7336131cb160a030ccd1874d370
CharlesPlachno <charlesplachno@outlook.com>
2022-06-22 21:53:32 -0700

    fixed two typos

--- test_runner/test_palindrome_number.py
+++ test_runner/test_palindrome_number.py
@@ -7,10 +7,10 @@ import pytest
 
 @pytest.mark.parametrize("palindrome", [1221, 1, 78901234567890123456789012345678900987654321098765432109876543210987])
 def test_positives(palindrome):
-    """takes paramatrized list of ints and asserts that they are palindromes as expected"""
+    """takes parametrized list of ints and asserts that they are palindromes as expected"""
     assert is_palindrome(palindrome)
 
 @pytest.mark.parametrize("not_palindrome", [123, 1000000000000000000000020000000000000000000000000001])
 def test_negatives(not_palindrome):
-    """takes paramatrized list of ints and asserts that they are not palindromes as expected"""
+    """takes parametrized list of ints and asserts that they are not palindromes as expected"""
     assert not is_palindrome(not_palindrome)
