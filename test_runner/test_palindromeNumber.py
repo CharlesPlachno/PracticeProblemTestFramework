0620584b861a96dadfc22f9f7197817081a497c0
CharlesPlachno <charlesplachno@outlook.com>
2022-06-22 21:09:17 -0700

    rewrote palindromes again to adopt parameterized testing

--- test_runner/test_palindromeNumber.py
+++ test_runner/test_palindromeNumber.py
@@ -1,13 +1,10 @@
 from problems.palindromeNumber import isPalindrome
+import pytest
 
-def test_normal():
-    assert isPalindrome(1221)
+@pytest.mark.parametrize("palindrome", [1221, 1, 12345678901234567890123456789012345678900987654321098765432109876543210987654321])
+def test_positives(palindrome):
+    assert isPalindrome(palindrome)
 
-def test_false():
-    assert not isPalindrome(123)
-
-def test_single():
-    assert isPalindrome(1)
-
-def test_long():
-    assert isPalindrome(12345678901234567890123456789012345678900987654321098765432109876543210987654321)
+@pytest.mark.parametrize("not_palindrome", [123, 1000000000000000000000020000000000000000000000000001])
+def test_negatives(not_palindrome):
+    assert not isPalindrome(not_palindrome)
