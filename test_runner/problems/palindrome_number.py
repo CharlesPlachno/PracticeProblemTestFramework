beaf55c8ff67dc63c3a7e1e879d51e5ff27ab3a0
CharlesPlachno <charlesplachno@outlook.com>
2022-06-22 21:42:02 -0700

    made changes based on lint results

--- test_runner/problems/palindrome_number.py
+++ test_runner/problems/palindrome_number.py
@@ -1,4 +1,3 @@
-
 '''
 Given an integer x, return true if x is palindrome integer.
 
@@ -8,11 +7,12 @@ For example, 121 is a palindrome while 123 is not.
 '''
 
 
-def isPalindrome(x: int) -> bool:
+def is_palindrome(x: int) -> bool:
+    """takes an int x, returns true if x is a palindrome, false if it is not"""
     text = str(x)
     for i in range(len(text)):
         #loop through index while checking against opposite index
-        if(text[i] != text[(-i) -1]):
+        if(text[i] != text[-i -1]):
             return False
     # Finished loop without returning false, return true
     return True
