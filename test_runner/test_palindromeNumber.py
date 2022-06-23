2a4a59d955c461d162fcc59bb1f8bd49ea9a4f91
CharlesPlachno <charlesplachno@outlook.com>
2022-06-22 20:58:25 -0700

    added to palindrome test

--- test_runner/test_palindromeNumber.py
+++ test_runner/test_palindromeNumber.py
@@ -1,5 +1,16 @@
-from problems.palindromeNumber import PalindromeNumber
+from problems.palindromeNumber import isPalindrome
 
-def test_pal():
-    palindromeTest = PalindromeNumber()
-    assert palindromeTest.isPalindrome("abba")
+def test_normal():
+    assert isPalindrome("abba")
+
+def test_empty():
+    assert isPalindrome("")
+
+def test_false():
+    assert not isPalindrome("false")
+
+def test_single():
+    assert isPalindrome("a")
+
+def test_long():
+    assert isPalindrome("ababababababababababababababababbabababababababababababababababa")
