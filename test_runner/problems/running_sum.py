8dce190ca17e24ba9bb1cb023fdd879e05ed69bb
CharlesPlachno <charlesplachno@outlook.com>
2022-08-01 16:53:21 -0700

    created test anagram checker

--- test_runner/problems/running_sum.py
+++ test_runner/problems/running_sum.py
@@ -5,11 +5,20 @@ Return the running sum of nums.
 '''
 
 def running_sums(nums):
-    """given a list of ints, return a list of ints"""
+    """
+    given a list of numbers, return a list of floats
+    if invalid input, return None
+    """
     # Iterate through nums keeping a tally of sum so far
     current_sum = 0
     run_sums = []
     for num in nums:
+        # Check valid value
+        try:
+            num = float(num)
+        except:
+            print("%s is not a valid number" % str(num))
+            return None
         current_sum += num
         run_sums.append(current_sum)
     return run_sums
--- test_runner/test_running_sum.py
+++ test_runner/test_running_sum.py
@@ -3,12 +3,12 @@ a set of input/output tests for the running_sums function
 '''
 
 from problems.running_sum import running_sums
+import pytest
 
-def test_example_one():
-    assert running_sums([1,2,3,4]) == [1,3,6,10]
+@pytest.mark.parametrize("nums1, nums2", [[[1,2,3,4.1], [1.0,3.0,6.0,10.1]], [[1,1.0,1,"1.5",1], [1.0,2.0,3.0,4.5,5.5]], [[3,1.5,-2.5,10,-.5], [3.0,4.5,2.0,12.0,11.5]], [[1,1,1,'a',1], None]])
+def test_positives(nums1, nums2):
+    assert running_sums(nums1) == nums2
 
-def test_example_two():
-    assert running_sums([1,1,1,1,1]) == [1,2,3,4,5]
-
-def test_example_three():
-    assert running_sums([3,1,2,10,1]) == [3,4,6,16,17]
+@pytest.mark.parametrize("nums1, nums2", [[[1,2,3,4], [1,3,6,11]]])
+def test_negatives(nums1, nums2):
+    assert running_sums(nums1) != nums2
