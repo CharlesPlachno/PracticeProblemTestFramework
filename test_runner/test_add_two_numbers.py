9e842226cc0efc98730c0fcd82577c1b5febd8bc
CharlesPlachno <charlesplachno@outlook.com>
2022-08-02 16:42:58 -0700

    Added negative black box tests. One test should be failing

--- test_runner/test_add_two_numbers.py
+++ test_runner/test_add_two_numbers.py
@@ -20,8 +20,23 @@ pl7 = number_to_sll(9999999)
 pl8 = number_to_sll(9999)
 pl9 = number_to_sll(10009998)
 
+nl1 = number_to_sll(0)
+nl2 = "abc"
+
+nl4 = number_to_sll(0)
+nl5 = 12
+
+
+nl7 = SingleListNode(134)
+nl8 = number_to_sll(134)
+
+
 @pytest.mark.parametrize("l1, l2, l3", [[pl1, pl2, pl3], [pl4, pl5, pl6], [pl7, pl8, pl9]])
 def test_positives(l1, l2, l3):
     assert addTwoNumbers(l1, l2) != None
     assert addTwoNumbers(l1, l2).to_int() == l3.to_int()
+
+@pytest.mark.parametrize("l1, l2", [[nl1, nl2], [nl4, nl5], [nl7, nl8]])
+def test_negatives(l1, l2):
+    assert addTwoNumbers(l1, l2) is None
 
